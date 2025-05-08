d1556e84b765e45f75d0e9a1227d25875a7abcd0
LucasBondeDeSouza <bondelucas77@gmail.com>
2025-05-08 14:19:45 -0300

    atualização

---
+++ backend/index.js
@@ -0,0 +1,56 @@
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import VendaMensal from "./VendaMensal.js"
+
+dotenv.config()
+
+const app = express()
+const PORT = 3000
+
+app.use(express.json())
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log('Conectado ao MongoDB')
+    } catch (err) {
+        console.log('Deu erro ao conectar com o MongoDB', err)
+    }
+}
+
+connectDB()
+
+// CREATE
+app.post("/vendas", async (req, res) => {
+    try {
+        const novaVendaMensal = await VendaMensal.create(req.body)
+        res.json(novaVendaMensal)
+    } catch (err) {
+        res.json(err)
+    }
+})
+
+// READ
+app.get("/vendas", async (req, res) => {
+    try {
+        const vendasMensais = await VendaMensal.find()
+        res.json(vendasMensais)
+    } catch (err) {
+        res.json(err)
+    }
+})
+
+// DELETE
+app.delete("/vendas/:id", async (req, res) => {
+    try {
+        const vendaMensalExcluida = await VendaMensal.findByIdAndDelete(req.params.id)
+        res.json(vendaMensalExcluida)
+    } catch (err) {
+        res.json(err)
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`O servidor está rodando na porta ${PORT}`)
+})
